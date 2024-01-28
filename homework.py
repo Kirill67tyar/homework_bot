6372312799891c34363e7e496d11f5d450fcae1f
Kirill Bogomolov <kirillbogomolov.ric@gmail.com>
2024-01-29 03:50:28 +0500

    Create base functions

--- homework.py
+++ homework.py
@@ -1,11 +1,21 @@
-...
+import os
+import time
+import json
+from pprint import pprint as pp
+
+import requests
+
+import telegram
+from telegram import Bot
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = None  # os.getenv('asd')
+TELEGRAM_CHAT_ID = None
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,27 +28,71 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+# Функция check_tokens() проверяет доступность переменных окружения,
+# которые необходимы для работы программы.
+# Если отсутствует хотя бы одна переменная окружения — продолжать работу бота нет смысла.
+
 
 def check_tokens():
-    ...
+    try:
+        assert PRACTICUM_TOKEN, 'PRACTICUM_TOKEN'
+        assert TELEGRAM_TOKEN, 'TELEGRAM_TOKEN'
+        assert TELEGRAM_CHAT_ID, 'TELEGRAM_CHAT_ID'
+    except AssertionError as err:
+        print('Not determine token:')
+        print(err)
+        raise SystemExit
 
 
+# Функция send_message() отправляет сообщение в Telegram чат, определяемый переменной окружения TELEGRAM_CHAT_ID.
+# Принимает на вход два параметра: экземпляр класса Bot и строку с текстом сообщения.
 def send_message(bot, message):
-    ...
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=message,
+    )
+
+
+# Функция get_api_answer() делает запрос к единственному эндпоинту API-сервиса.
+# В качестве параметра в функцию передается временная метка.
+# В случае успешного запроса должна вернуть ответ API, приведя его из формата JSON к типам данных Python.
 
 
 def get_api_answer(timestamp):
-    ...
+    params = {
+        'from_date': timestamp,
+    }
+    response = requests.get(
+        ENDPOINT,
+        headers=HEADERS,
+        params=params
+    )
+    if response.ok:
+        return response.json()
+
+# pp(get_api_answer(int(time.time())))
 
 
-def check_response(response):
-    ...
+def write_in_json():
+    with open('resp.json', 'w', encoding='utf-8') as f:
+        json.dump(get_api_answer(0), f, ensure_ascii=False)
 
 
-def parse_status(homework):
-    ...
+# Функция check_response() проверяет ответ API на соответствие документации
+# из урока API сервиса Практикум.Домашка.
+# В качестве параметра функция получает ответ API, приведенный к типам данных Python.
+# def check_response(response):
+#     ...
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+# # !-----------------------------
+# # Функция parse_status() извлекает из информации о конкретной домашней работе статус этой работы.
+# # В качестве параметра функция получает только один элемент из списка домашних работ.
+# # В случае успеха, функция возвращает подготовленную для отправки в Telegram строку,
+# # содержащую один из вердиктов словаря HOMEWORK_VERDICTS.
+# def parse_status(homework):
+#     ...
+
+#     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
@@ -46,21 +100,26 @@ def main():
 
     ...
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    # bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    get_api_answer(timestamp)
+    return get_api_answer(timestamp)  # ! ----------
+    # send_message(bot)
 
     ...
 
-    while True:
-        try:
+    # while True:
+    #     try:
 
-            ...
+    #         ...
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+    #     except Exception as error:
+    #         message = f'Сбой в работе программы: {error}'
+    #         ...
+    #     ...
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(main())
+    pass
